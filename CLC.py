1e36b21e7b2fbd78f51d56b824d422c1d18fb2df
Neha <neha.sharma@hqvfx.in>
2023-01-27 11:08:24 -0330

    my_cal.

---
+++ CLC.py
@@ -0,0 +1,30 @@
+def add(a, b):
+    c = a + b
+    return c
+
+
+def sub(p, q):
+    r = p - q
+    return r
+
+
+def mul(d, e):
+    f = d * e
+    return f
+
+
+def div(t, s):
+    u = s / t
+    return u
+
+
+def exp(g, h):
+    i = g ** h
+    return i
+
+
+print(add(12, 84))
+print(sub(85, 75))
+print(mul(74, 87))
+print(div(45, 87))
+print(exp(5, 4))
